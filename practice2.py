bcfdac75e140cdf45e769bda32950afceb95acbf
GodasuSrikanth <117339572+GodasuSrikanth@users.noreply.github.com>
2022-11-07 14:32:29 +0530

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,19 @@
+#for language in "python":
+   #print(language)
+
+prices = [10,20,30]
+total=0
+for price in prices:
+    total += price
+    print(f"Total: {total}")
+
+
+numbers=[2,3,1,4,5]
+for x_count in numbers:
+    output = ""
+    for count in range(x_count):
+        output += "x"
+        print(output)
+
+
+ 
