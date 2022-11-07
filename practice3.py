8112cf15ece1403906e411b13d79e0936a26fc17
GodasuSrikanth <117339572+GodasuSrikanth@users.noreply.github.com>
2022-11-07 14:32:59 +0530

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,16 @@
+#find the largest number 
+'''numbers=[4,5,3,6,10]
+max= numbers[0]
+for number in numbers:
+  if number>max:
+       max=number
+print(max)'''
+
+
+#remove the duplicates in the list
+numbers=[2,4,5,3,5,4,6]
+uniques=[]
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+        print(uniques)
